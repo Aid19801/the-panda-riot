daf6dce50b5e9ad8e1d1edf2a26480f2b457f947
Adrian Thompson <adrian.thompson@infabode.com>
2019-04-24 05:50:02 +0100

    news section looking good

--- src/components/Box-Card/index.js
+++ src/components/Box-Card/index.js
@@ -5,8 +5,8 @@ import './styles.scss';
 const BoxCard = ({ img, blurb, headline, link, src }) => (
     <div className="box-card" onClick={() => window.open(link,'_newtab')}>
         <h4 className="card-h4">{trimStringSpecifically(headline, 55)}</h4>
-        <h3 className="card-h3" >{src}</h3>
         <p className="card-p">{trimStringSpecifically(blurb, 110)}</p>
+        <h3 className="card-h3" >{src}</h3>
 
         <div className="card-img-container">
             <img className="card-img" src={img} />
