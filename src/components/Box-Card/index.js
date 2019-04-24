919bb85709bbcd28ad29e08d1c6061997d3de3f8
Adrian Thompson <adrian.thompson@infabode.com>
2019-04-24 06:07:45 +0100

    news section looking good

--- src/components/Box-Card/index.js
+++ src/components/Box-Card/index.js
@@ -3,7 +3,7 @@ import { trimStringSpecifically } from '../../lib/utils';
 import './styles.scss';
 
 const BoxCard = ({ img, blurb, headline, link, src }) => (
-    <div className="box-card" onClick={() => window.open(link,'_newtab')}>
+    <div className="box-card hvr-float-shadow" onClick={() => window.open(link,'_newtab')}>
         <h4 className="card-h4">{trimStringSpecifically(headline, 55)}</h4>
         <p className="card-p">{trimStringSpecifically(blurb, 110)}</p>
         <h3 className="card-h3" >{src}</h3>
