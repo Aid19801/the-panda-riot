bd3f4aeed38b0e033158382d7f5e6bca784ef029
Adrian Thompson <adrian.thompson@infabode.com>
2019-05-02 09:10:33 +0100

    node vers added

---
+++ src/components/AdvertBox/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './styles.scss';
+
+function AdvertBox({ link, src, text }) {
+  return (
+    <div className="div__advert-box margin-top padding-on" onClick={() => window.open(link, '_newtab')}>
+      <div className="img-container">
+        <img className="advert-img" src={src} />
+      </div>
+        <h4>{text}</h4>
+    </div>
+  )
+}
+
+export default AdvertBox;
