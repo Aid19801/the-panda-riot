fd911f50df37c465782f2b968e45769958f6a721
Adrian Thompson <adrian.thompson@infabode.com>
2019-06-01 13:57:46 +0200

    added stackable LIVE TONIGHT compoennts

--- src/components/GigOnTonight/index.js
+++ src/components/GigOnTonight/index.js
@@ -2,16 +2,29 @@ import React from 'react';
 import Col from 'react-bootstrap/Col';
 import './styles.scss';
 
-export default function GigOnTonight({ name, nearestTubes, img, bringer }) {
+export default function GigOnTonight({ id, name, nearestTubes, img, bringer }) {
+
+    const calcAngle = () => {
+        let num = Math.floor(Math.random() * 15) + 1;
+        let oneOrTwo = Math.floor(Math.random() * 2) + 1;
+        let plusOrMinus = oneOrTwo === 1 ? '-' : '+';
+        return `${plusOrMinus}${num}`;    
+    }
+    
+    console.log(calcAngle());
     return (
-        <Col sm={4}>
-            <div className="div__gig-tonight-container margin-top">
-                <p>Tonight!</p>
-                <h4>{name}</h4>
-                <p>{nearestTubes && nearestTubes[0]}</p>
-                { bringer && <p>bringer</p>}
+        <div className="col-sm-3 div__each-gig-tonight">
+
+
+            <img className="img__gig-tonight-img" src={img} />
+
+
+            <div className="div__flex-stack-col">
+                <h4 className="h4__gig-tonight-name">{name}</h4>
+                <p className="p__gig-tonight-tubes">{nearestTubes && nearestTubes[0]}</p>
+                { bringer && <p className="p__gig-tonight-bringer">bringer</p>}
             </div>
-        </Col>
+        </div>
     )
 }
 
--- src/components/GigsTonightContainer/index.js
+++ src/components/GigsTonightContainer/index.js
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import { whatDayIsIt } from '../../lib/utils';
-import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Gists from 'gists';
+import './styles.scss';
 
 import GigOnTonight from '../GigOnTonight';
+import { Spinner } from '..';
 
 
  let mockProps = {
@@ -24,19 +25,6 @@ const GigsOnTonightContainer = () => {
         fetchGigs()
     }, [])
 
-    const sortGigs = (a, b) => {
-        console.log('AT | sortGigs fired');
-        const nameA = a.name.toUpperCase();
-        const nameB = b.name.toUpperCase();
-        let comparison = 0;
-        if (nameA > nameB) {
-            comparison = 1;
-        } else if (nameA < nameB) {
-            comparison = -1;
-        }
-        return comparison;
-    }
-
     const fetchGigs = async () => {
         try {
             let rawURL = await gists.get('7c88e1645fd8518999fb9c764c0d1869')
@@ -49,7 +37,7 @@ const GigsOnTonightContainer = () => {
                 .catch(err => console.log('error fetching gigs 1.1:', err))
 
                 console.log('today is:',today)
-                let tonightsGigs = await allGigs.gigs.filter(each => each.nights.includes(today) === true);
+                let tonightsGigs = await allGigs.gigs.filter(each => each.nights.includes("Wed") === true);
                 console.log('AT | tonightsGigs: ', tonightsGigs);
                 setGigs(tonightsGigs);
 
@@ -59,12 +47,17 @@ const GigsOnTonightContainer = () => {
     }
 
     return (
-        <Row>
+
+        <div className="div__gigs-tonight-section row fluid padding-on margin-on">
+            { !gigs || gigs.length === 0 && (
+                <Spinner />
+            )}
             { gigs && gigs.length > 0 && 
                 gigs.map((each, i) => {
                     return (
                         <GigOnTonight
                             key={each.id}
+                            id={i+1}
                             nearestTubes={each.nearestTubes}
                             name={each.name}
                             bringer={each.bringer}
@@ -73,7 +66,7 @@ const GigsOnTonightContainer = () => {
                     )
                 })
             }
-        </Row>
+        </div>
     )
 }
 
