18ce874a07ab9886f44a8d021b4f8941d6025e11
Adrian Thompson <adrian.thompson@infabode.com>
2019-06-12 20:28:25 +0100

    navbar loading fixes

--- src/components/GigOnTonight/index.js
+++ src/components/GigOnTonight/index.js
@@ -14,7 +14,11 @@ export default function GigOnTonight({ id, venue, name, nearestTubes, img, bring
                         <h4 className="h4__gig-tonight margin-off">{name}</h4>
                         <p className="white margin-off">@ {venue}</p>
                     </div>
-                    <div className="col-sm-7">
+                    <div className="col-sm-7 div__stack-underground">
+                        <img
+                            className="img__underground"
+                            src="https://cdn0.iconfinder.com/data/icons/usefull-geo-points-for-maps/512/london-metro-metropolitan-underground-label-sign-512.png"
+                        />
                         <p className="p__gig-tonight-info white margin-off">{nearestTubes[0]}</p>
                     </div>
                 </div>
