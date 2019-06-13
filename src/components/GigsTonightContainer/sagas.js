4c0ef86fa92c2185560527c898fafe734320b72e
Adrian Thompson <adrian.thompson@infabode.com>
2019-06-13 16:52:23 +0100

    fixed

---
+++ src/components/GigsTonightContainer/sagas.js
@@ -0,0 +1,42 @@
+import { takeLatest, put } from 'redux-saga/effects';
+import Gists from 'gists';
+
+import { whatDayIsIt } from '../../lib/utils';
+
+import * as actions from './constants';
+
+export function* watcherFetchGigsTonight() {
+    yield takeLatest(actions.FETCH_GIGS_TONIGHT, workerFetchGigsTonight);
+}
+
+function* workerFetchGigsTonight() {
+    console.log('heard fetch gigs tonight');
+
+    let rawURL;
+    let error;
+    let retrievedGigs;
+
+    // go to gist
+    yield fetch('https://api.github.com/gists/7c88e1645fd8518999fb9c764c0d1869')
+        .then(res => res.json())
+        .then(json => {
+            return rawURL = json.files.gigs.raw_url;
+        })
+        .catch(err => error = err);
+    
+    // get dirty raw url for all the gigs
+    yield fetch(rawURL)
+        .then(res => res.json())
+        .then(json => {
+            let today = whatDayIsIt();
+            retrievedGigs = json.gigs.filter(each => each.nights.includes(today) === true);
+            return retrievedGigs;
+        })
+        .catch(err => console.log('saga | Fetch gigs 2nite error: ', err))
+
+        retrievedGigs.length > 0 ?
+        yield put({ type: actions.FETCH_GIGS_SUCCESS, gigs: retrievedGigs })
+        :
+        yield put({ type: actions.FETCH_GIGS_FAILED, error: error })
+
+}
--- src/redux/sagas.js
+++ src/redux/sagas.js
@@ -1,3 +1,4 @@
+import { watcherFetchGigsTonight } from '../components/GigsTonightContainer/sagas';
 import { watcherFetchGigs } from '../containers/data-map-page/sagas';
 import { watcherFetchFilters } from '../containers/data-map-page/sagas';
 import { watcherUserFilteringGigs } from '../containers/data-map-page/sagas';
@@ -6,6 +7,7 @@ import { watcherFetchNews } from '../containers/homepage/sagas';
 
 export {
     watcherFetchGigs,
+    watcherFetchGigsTonight,
     watcherFetchFilters,
     watcherFetchNews,
     watcherUserFilteringGigs,
