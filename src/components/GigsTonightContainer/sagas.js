2fe873f78d87c62194137bfab978e0b18caef4d3
Adrian Thompson <adrian.thompson@infabode.com>
2019-06-13 21:02:03 +0100

    gigs tonight should work now

--- src/components/GigsTonightContainer/sagas.js
+++ src/components/GigsTonightContainer/sagas.js
@@ -17,7 +17,7 @@ function* workerFetchGigsTonight() {
     let retrievedGigs;
 
     // go to gist
-    yield fetch('https://api.github.com/gists/7c88e1645fd8518999fb9c764c0d1869')
+    yield fetch(`https://api.github.com/gists/${process.env.REACT_APP_GIG_GIST}`)
         .then(res => res.json())
         .then(json => {
             return rawURL = json.files.gigs.raw_url;
