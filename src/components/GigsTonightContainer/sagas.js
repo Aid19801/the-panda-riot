2dc6f773b2b5e4c74b46fd56b04d710a4ce4071c
Adrian Thompson <adrian.thompson@infabode.com>
2019-06-13 21:09:25 +0100

    gigs tonight should work now

--- src/components/GigsTonightContainer/sagas.js
+++ src/components/GigsTonightContainer/sagas.js
@@ -1,8 +1,5 @@
 import { takeLatest, put } from 'redux-saga/effects';
-import Gists from 'gists';
-
 import { whatDayIsIt } from '../../lib/utils';
-
 import * as actions from './constants';
 
 export function* watcherFetchGigsTonight() {
@@ -16,27 +13,27 @@ function* workerFetchGigsTonight() {
     let error;
     let retrievedGigs;
 
-    // go to gist
-    yield fetch(`https://api.github.com/gists/${process.env.REACT_APP_GIG_GIST}`)
-        .then(res => res.json())
-        .then(json => {
-            return rawURL = json.files.gigs.raw_url;
-        })
-        .catch(err => error = err);
+    // // go to gist
+    // yield fetch(`https://api.github.com/gists/${process.env.REACT_APP_GIG_GIST}`)
+    //     .then(res => res.json())
+    //     .then(json => {
+    //         return rawURL = json.files.gigs.raw_url;
+    //     })
+    //     .catch(err => error = err);
     
-    // get dirty raw url for all the gigs
-    yield fetch(rawURL)
-        .then(res => res.json())
-        .then(json => {
-            let today = whatDayIsIt();
-            retrievedGigs = json.gigs.filter(each => each.nights.includes(today) === true);
-            return retrievedGigs;
-        })
-        .catch(err => console.log('saga | Fetch gigs 2nite error: ', err))
+    // // get dirty raw url for all the gigs
+    // yield fetch(rawURL)
+    //     .then(res => res.json())
+    //     .then(json => {
+    //         let today = whatDayIsIt();
+    //         retrievedGigs = json.gigs.filter(each => each.nights.includes(today) === true);
+    //         return retrievedGigs;
+    //     })
+    //     .catch(err => console.log('saga | Fetch gigs 2nite error: ', err))
 
-        retrievedGigs.length > 0 ?
-        yield put({ type: actions.FETCH_GIGS_SUCCESS, gigs: retrievedGigs })
-        :
-        yield put({ type: actions.FETCH_GIGS_FAILED, error: error })
+    //     retrievedGigs.length > 0 ?
+    //     yield put({ type: actions.FETCH_GIGS_SUCCESS, gigs: retrievedGigs })
+    //     :
+    //     yield put({ type: actions.FETCH_GIGS_FAILED, error: error })
 
 }
