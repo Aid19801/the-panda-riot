5af839a25b45811ec48f8f35d3cb971977c54754
Adrian Thompson <adrian.thompson@infabode.com>
2019-06-20 21:06:27 +0100

    release 20062019

--- src/components/GigOnTonight/index.js
+++ src/components/GigOnTonight/index.js
@@ -1,88 +1,88 @@
-// import React from 'react';
-// import { withRouter } from 'react-router-dom'
-// import { connect } from 'react-redux';
-// import { compose } from 'redux';
-// import * as ROUTES from '../../constants/routes';
-// import { whatDayIsIt } from '../../lib/utils';
-// // import * as actions from './constants';
+import React from 'react';
+import { withRouter } from 'react-router-dom'
+import { connect } from 'react-redux';
+import { compose } from 'redux';
+import * as ROUTES from '../../constants/routes';
+import { whatDayIsIt } from '../../lib/utils';
+// import * as actions from './constants';
 
-// import './styles.scss';
+import './styles.scss';
 
-// function GigOnTonight({ toggleMarker, userFiltered, filters, allInfo, venue, name, nearestTubes, img, bringer, history }) {
+function GigOnTonight({ toggleMarker, userFiltered, filters, allInfo, venue, name, nearestTubes, img, bringer, history }) {
 
-//     let newPaneInfo = {
-//         heading: name,
-//         subheading: `@ ${venue}`,
-//         paragraph: allInfo.blurb,
-//         nights: allInfo.nights,
-//         img: img,
-//         lng: allInfo.lng,
-//         lat: allInfo.lat,
-//         walkins: allInfo.walkins,
-//         walkinSignUp: allInfo.walkinSignUp,
-//         prebook: allInfo.prebook,
-//         prebookSignUp: allInfo.prebookSignUp,
-//         bringer: bringer,
-//         nearestTubes: nearestTubes,
-//         twitterHandle: allInfo.twitterHandle,
-//         website: allInfo.website,
-//         howToBook: allInfo.howToBook,
-//         venue: venue,
-//     }
+    let newPaneInfo = {
+        heading: name,
+        subheading: `@ ${venue}`,
+        paragraph: allInfo.blurb,
+        nights: allInfo.nights,
+        img: img,
+        lng: allInfo.lng,
+        lat: allInfo.lat,
+        walkins: allInfo.walkins,
+        walkinSignUp: allInfo.walkinSignUp,
+        prebook: allInfo.prebook,
+        prebookSignUp: allInfo.prebookSignUp,
+        bringer: bringer,
+        nearestTubes: nearestTubes,
+        twitterHandle: allInfo.twitterHandle,
+        website: allInfo.website,
+        howToBook: allInfo.howToBook,
+        venue: venue,
+    }
 
-//     const selectTheDayInFilters = (day) => { 
-//         let today = whatDayIsIt();
-//         let chosenFilter = filters.filter(each => each.filterName === today)[0];
-//         console.log('AT | chosen Filter: ', chosenFilter);
-//         let updatedFilter = {
-//             ...chosenFilter,
-//             active: true,
-//         }
+    // const selectTheDayInFilters = (day) => { 
+    //     let today = whatDayIsIt();
+    //     let chosenFilter = filters.filter(each => each.filterName === today)[0];
+    //     console.log('AT | chosen Filter: ', chosenFilter);
+    //     let updatedFilter = {
+    //         ...chosenFilter,
+    //         active: true,
+    //     }
 
-//         console.log('AT | updatedFilter: ', updatedFilter);
-//         userFiltered(updatedFilter);
+    //     console.log('AT | updatedFilter: ', updatedFilter);
+    //     userFiltered(updatedFilter);
 
-//         return updatedFilter;
-//     }
-//     const reRouteToMap = () => {
-//         // selectTheDayInFilters();
-//         toggleMarker(newPaneInfo);
-//         history.push('/datamap');
-//     }
+    //     return updatedFilter;
+    // }
+    const reRouteToMap = () => {
+        // selectTheDayInFilters();
+        toggleMarker(newPaneInfo);
+        history.push('/datamap');
+    }
 
-//     return (
-//         <div onClick={() => reRouteToMap()} className="col-sm-3 div__each-gig-tonight">
+    return (
+        <div onClick={() => reRouteToMap()} className="col-sm-3 div__each-gig-tonight">
 
-//             <div className="div__inner-box">
-//                 <div className="row">
-//                     <img  className="img__gig-tonight-img" src={img} />
-//                     <div className="col-sm-7 gig-name">
-//                         <h4 className="h4__gig-tonight margin-off">{name}</h4>
-//                         <p className="white margin-off">@ {venue}</p>
-//                     </div>
-//                     <div className="col-sm-7 div__stack-underground">
-//                         <img
-//                             className="img__underground"
-//                             src="https://cdn0.iconfinder.com/data/icons/usefull-geo-points-for-maps/512/london-metro-metropolitan-underground-label-sign-512.png"
-//                         />
-//                         <p className="p__gig-tonight-info white margin-off">{nearestTubes[0]}</p>
-//                     </div>
-//                 </div>
-//             </div>
-//         </div>
-//     )
-// }
+            <div className="div__inner-box">
+                <div className="row">
+                    <img  className="img__gig-tonight-img" src={img} />
+                    <div className="col-sm-7 gig-name">
+                        <h4 className="h4__gig-tonight margin-off">{name}</h4>
+                        <p className="white margin-off">@ {venue}</p>
+                    </div>
+                    <div className="col-sm-7 div__stack-underground">
+                        <img
+                            className="img__underground"
+                            src="https://cdn0.iconfinder.com/data/icons/usefull-geo-points-for-maps/512/london-metro-metropolitan-underground-label-sign-512.png"
+                        />
+                        <p className="p__gig-tonight-info white margin-off">{nearestTubes[0]}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
 
 
-// const mapStateToProps = state => ({
-//     filters: state.dataMapPage.filters,
-// })
+const mapStateToProps = state => ({
+    filters: state.dataMapPage.filters,
+})
 
-// const mapDispatchToProps = dispatch => ({
-//     toggleMarker: (paneInfo) => dispatch({ type: 'USER_CLICKED_MARKER', paneInfo }),
-//     userFiltered: (chosenFilter) => dispatch({ type: 'USER_CLICKED_FILTER', filter: chosenFilter }),
-// })
-// export default compose(
-//     withRouter,
-//     connect(mapStateToProps, mapDispatchToProps),
-// )(GigOnTonight)
+const mapDispatchToProps = dispatch => ({
+    toggleMarker: (paneInfo) => dispatch({ type: 'USER_CLICKED_MARKER', paneInfo }),
+    userFiltered: (chosenFilter) => dispatch({ type: 'USER_CLICKED_FILTER', filter: chosenFilter }),
+})
+export default compose(
+    withRouter,
+    connect(mapStateToProps, mapDispatchToProps),
+)(GigOnTonight)
--- src/components/GigsTonightContainer/index.js
+++ src/components/GigsTonightContainer/index.js
@@ -2,46 +2,44 @@ import React, { useState, useEffect } from 'react';
 import { connect } from 'react-redux';
 import './styles.scss';
 
-// import GigOnTonight from '../GigOnTonight';
+import GigOnTonight from '../GigOnTonight';
 
-const GigsOnTonightContainer = () => <div className="foo">news</div>;
-export default GigsOnTonightContainer;
-// const GigsOnTonightContainer = ({ fetchGigsTonight, gigs }) => {
+const GigsOnTonightContainer = ({ fetchGigsTonight, gigs }) => {
 
-//     useEffect(() => {
-//         fetchGigsTonight()
-//     }, [])
+    useEffect(() => {
+        fetchGigsTonight()
+    }, [])
 
-//     return (
+    return (
 
-//         <div className="div__gigs-tonight-section row fluid padding-on margin-on">
-//             <h4 className="col-sm-12 center">Happening Tonight</h4>
+        <div className="div__gigs-tonight-section row fluid padding-on margin-on">
+            <h4 className="col-sm-12 center">Happening Tonight</h4>
 
-//             { gigs && gigs.length > 0 && 
-//                 gigs.map((each, i) => {
-//                     return (
-//                         <GigOnTonight
-//                             key={each.id}
-//                             id={i+1}
-//                             nearestTubes={each.nearestTubes}
-//                             name={each.name}
-//                             bringer={each.bringer}
-//                             img={each.img}
-//                             venue={each.venue}
-//                             allInfo={each}
-//                         />
-//                     )
-//                 })
-//             }
-//         </div>
-//     )
-// }
-// const mapStateToProps = (state) => ({
-//     gigs: state.fetchGigsTonight.gigs,
-// })
+            { gigs && gigs.length > 0 && 
+                gigs.map((each, i) => {
+                    return (
+                        <GigOnTonight
+                            key={each.id}
+                            id={i+1}
+                            nearestTubes={each.nearestTubes}
+                            name={each.name}
+                            bringer={each.bringer}
+                            img={each.img}
+                            venue={each.venue}
+                            allInfo={each}
+                        />
+                    )
+                })
+            }
+        </div>
+    )
+}
+const mapStateToProps = (state) => ({
+    gigs: state.fetchGigsTonight.gigs,
+})
 
-// const mapDispatchToProps = (dispatch) => ({
-//     fetchGigsTonight: () => dispatch({ type: 'FETCH_GIGS_TONIGHT' })
-// })
+const mapDispatchToProps = (dispatch) => ({
+    fetchGigsTonight: () => dispatch({ type: 'FETCH_GIGS_TONIGHT' })
+})
 
-// export default connect(mapStateToProps, mapDispatchToProps)(GigsOnTonightContainer);
+export default connect(mapStateToProps, mapDispatchToProps)(GigsOnTonightContainer);
