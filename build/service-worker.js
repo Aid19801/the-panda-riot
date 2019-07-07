39c476409843613e87875220b28da4c291291749
Adrian Thompson <adrian.thompson@infabode.com>
2019-07-07 08:27:58 +0100

    fb twitter meta tags

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c9057a8e6e032a40755a10845b37aca2.js"
+  "/precache-manifest.9825aeebfe3842b6d8124e77c1a563b5.js"
 );
 
 workbox.clientsClaim();
